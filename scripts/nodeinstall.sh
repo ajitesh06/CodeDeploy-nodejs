266a44bc7e15652faa9044fce4b7d3fe816cbc82
Ajitesh Vennamaneni <avennama@kent.edu>
2017-06-16 23:11:59 -0400

    updated

--- scripts/nodeinstall.sh
+++ scripts/nodeinstall.sh
@@ -4,3 +4,6 @@ curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | b
 nvm install 4.4.5
 npm install forever -g
 echo "node installed"
+rm -rf /home/ec2-user/nodejs
+mkdir /home/ec2-user/nodejs
+sudo ln -s -f /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deplyment-archive /home/ec2-user/nodejs
